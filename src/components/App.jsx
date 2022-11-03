fb9af88f012e21e97edf13712951fa536da60496
snehal-joshi <59516532+snehal-joshi@users.noreply.github.com>
2022-11-02 20:40:30 -0500

    updates in the react components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,17 +4,11 @@ import Footer from "./Footer";
 import Note from "./Note";
 
 function App() {
-  var year = new Date().getFullYear();
-
   return (
     <div className="App">
-      <header>
-        <h1>Keeper</h1>
-      </header>
+      <Header />
       <Note />
-      <footer>
-        <p>Copyright {year}</p>
-      </footer>
+      <Footer />
     </div>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Footer() {
-  return <footer></footer>;
+  const year = new Date().getFullYear();
+
+  return (
+    <footer>
+      <p>Copyright © {year}</p>
+    </footer>
+  );
 }
 
 export default Footer;
